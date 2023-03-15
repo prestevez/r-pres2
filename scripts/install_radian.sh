28eb68e5c03fbf3d260e8d98f0406c96d2fa9f1e
Patricio R Estevez-Soto <patricio.estevez@ucl.ac.uk>
2023-03-15 23:18:45 +0000

    added tests to see if radian was adequately installed

--- scripts/install_radian.sh
+++ scripts/install_radian.sh
@@ -57,4 +57,7 @@ echo -e "Check the Python to use with reticulate...\n"
 
 R -q -e 'reticulate::py_discover_config(required_module = NULL, use_environment = NULL)'
 
+which radian
+radian --version
+
 echo -e "\nInstall Python + radian, done!"
